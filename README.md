5c133e7eb8bee71ec7aaea8294da8c174593b01c
lowbob84 <chris@lowbob.de>
2023-12-11 09:26:58 +0100

    Add Scripts and Readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wsl_ubuntu_and_kali_desktop_autoinstall
-Automatic install of WSL ubuntu desktop and kali linux desktop 
+Automatic install of WSL ubuntu desktop and kali linux desktop
+Found on different Websites, Posts the way to go for install the gnome-desktop for ubuntu in WSL.
+For kali linux i changed only the Destop variables and it worked
+
+# Please don't hesitate to contact me if you find some issues or know how to expand the code 
+
+## For x11 i used gwsl from Microsoft store, but other window Managers also should work  
+
+## 1. Configuration of WSL and start desktop
+1. Install gwsl form Microsoft store and start it
+2. Install clean ubuntu, ubuntu-22.04 or kali-linux from microsoft Store 
+3. Start WSL, create User and set password
+4. su
+5. copy script or make a new file with the code inside
+6. chmod +x ./wsl_ubuntu_install.sh
+7. ./wsl_ubuntu_install.sh
+8. after the script is finished shutdown the WSL in Powershell. wsl --shutdown Ubuntu-22.04 or wsl --shutdown kali-linux
+9. start ubuntu or kali-linux and start the desktop "start_kali_desktop" or "./start_ubuntu_dekstop.sh"
+
+## Think not all Desktop variables are right for kali-linux but desktop is working
+
---
+++ wsl_ubuntu_install.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+# title                   : "wsl_kali_install.sh"
+# description             : "Automatic install of wsl ubuntu and ubuntu 22.04 with gnome desktop"
+# author                  : "Christopher Lange aka. lowbob84"
+# git repository  		    : "https://github.com/lowbob84/"
+# email                   : "chris@lowbob.de"
+# date                    : 2023-12-11
+# version                 : 1.0.0
+
+# For updates and contributions, head over to
+# git repository  		: "https://github.com/lowbob84/wsl_ubuntu_and_kali_desktop_autoinstall"
+USER="$(grep 1000 /etc/passwd | awk -F: '{print $1}')"
+
+cat <<EOF > "/etc/sudoers.d/${USER}"
+${USER}  ALL=(ALL) NOPASSWD: ALL
+EOF
+
+## force apt ipv4
+#sudo echo 'Acquire::ForceIPv4 "true";' > /etc/apt/apt.conf.d/90force-ipv4
+
+apt-get update &&
+apt-get upgrade -qq -y &&
+apt-get install -qq -y net-tools bash-completion figlet
+
+cat <<EOF > "/etc/wsl.conf"
+[boot]
+systemd=true
+
+EOF
+
+cat <<\EOF >> "/home/${USER}/.bashrc"
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/cxoffice/bin:/snap/bin
+
+export LIBGL_ALWAYS_INDIRECT=1
+export DISPLAY=$(ip route list |grep default |awk '{print $3}'):0.0
+export PULSE_SERVER=tcp:$(ip route list |grep default |awk '{print $3}')
+EOF
+
+cat <<\EOF >> "/home/${USER}/.profile"
+export LIBGL_ALWAYS_INDIRECT=1
+export DISPLAY=$(ip route list |grep default |awk '{print $3}'):0.0
+export PULSE_SERVER=tcp:$(ip route list |grep default |awk '{print $3}')
+
+if [ -f ~/.dbus_init ]; then
+    . ~/.dbus_init
+fi
+
+if [ -f ~/.gnome_desktop_env ]; then
+    . ~/.gnome_desktop_env
+fi
+EOF
+
+cat <<EOF > "/home/${USER}/.gnome_desktop_env"
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME
+export XDG_SESSION_DESKTOP=ubuntu
+export DESKTOP_SESSION=ubuntu
+export GNOME_SHELL_SESSION_MODE=ubuntu
+
+# X11 sessions
+export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg
+export XDG_DATA_DIRS=/usr/share/ubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+export XDG_MENU_PREFIX=gnome-
+
+export XDG_SESSION_TYPE=x11
+export XDG_SESSION_CLASS=user
+export GDK_BACKEND=x11
+
+# Disables using Direct3D in Mesa 3D graphics library
+export LIBGL_ALWAYS_SOFTWARE=1
+EOF
+
+cat <<\EOF > "/home/${USER}/.dbus_init"
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+if [ ! -d "$XDG_RUNTIME_DIR" ]; then
+{
+    mkdir $XDG_RUNTIME_DIR && chmod 700 $XDG_RUNTIME_DIR && chown $(id -un):$(id -gn) $XDG_RUNTIME_DIR
+	mkdir $XDG_RUNTIME_DIR2 && chmod 700 $XDG_RUNTIME_DIR2 && chown root:root $XDG_RUNTIME_DIR
+    service dbus start
+}
+fi
+
+set_session_dbus()
+{
+    local bus_file_path="$XDG_RUNTIME_DIR/bus"
+
+    export DBUS_SESSION_BUS_ADDRESS=unix:path=$bus_file_path
+
+    if [ ! -e "$bus_file_path" ]; then
+    {
+        /usr/bin/dbus-daemon --session --address=$DBUS_SESSION_BUS_ADDRESS --nofork --nopidfile --syslog-only &
+    }
+    fi
+}
+
+set_session_dbus
+
+EOF
+
+cat <<EOF > "/etc/rc.local"
+#!/bin/bash
+mount -o rw,remount /tmp/.X11-unix/
+chmod +t /tmp/.X11-unix
+chmod o+rw /dev/dri/renderD128
+/usr/libexec/at-spi-bus-launcher --launch-immediately &
+EOF
+
+cat <<EOF > "/home/${USER}/start_ubuntu_dekstop.sh"
+#!/bin/bash
+if [ -z "$DISPLAY" ]; then
+    echo "Error: DISPLAY environment variable is not set."
+    exit
+fi
+
+gnome-session --session=ubuntu
+EOF
+
+chmod 755 "/home/${USER}/start_ubuntu_dekstop.sh"
+cd "/home/${USER}"
+chown "${USER}":"${USER}" .profile .gnome_desktop_env .dbus_init .bashrc
+service dbus start &&
+
+apt-mark hold acpid acpi-support modemmanager &&
+apt-get install -y ubuntu-gnome-desktop &&
+
+systemctl disable dbus-org.freedesktop.network1.service &&
+systemctl set-default multi-user.target &&
+sudo -u "${USER}" settings set org.gnome.desktop.interface enable-animations false
+
+N=5; while [[ $((--N)) >  0 ]]; do  echo -e "\033[2J\033[0m"; echo "Run start_ubuntu_dekstop.sh after reboot in $N seconds" |  figlet -c && sleep 1 ; done
